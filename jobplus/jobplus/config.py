8c208095538f245f55dbb16c956b3c256e367531
1021188397 <1021188397@qq.com>
2018-02-12 15:45:42 +0800

    modify config

--- jobplus/jobplus/config.py
+++ jobplus/jobplus/config.py
@@ -2,6 +2,7 @@ class BaseConfig(object):
     SECRET_KEY='makesure to set very secret key'
 
 class DevelopmentConfig(BaseConfig):
+    """开发"""
     DEBUG=True
     SQLALCHEMY_DATABASE_URI='mysql+mysqldb://root@localhost:3306/jobplus4-5?charset=utf8'
 
