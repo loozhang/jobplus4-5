e3cb5a178a873d92737b762830a2c041bdeebfe5
loozhang <loozhang@163.com>
2018-03-07 23:18:53 +0800

    Revert "fix #27 edit company.py"
    This reverts commit f963cc168a5e83c9d5f1e6bfc6fa599ef76a7c14.

--- jobplus/jobplus/handlers/job.py
+++ jobplus/jobplus/handlers/job.py
@@ -1,4 +1,4 @@
-""" job detail,job list """
+""" 职位详情，职位列表"""
 from flask import Blueprint, render_template, redirect, url_for, flash, request, current_app
 from jobplus.models import  User,Job
 from jobplus.forms import LoginForm,RegisterForm,CompanyRegisterForm
