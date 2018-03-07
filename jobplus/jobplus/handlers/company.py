f963cc168a5e83c9d5f1e6bfc6fa599ef76a7c14
loozhang <loozhang@163.com>
2018-03-07 23:01:02 +0800

    fix #27 edit company.py

--- jobplus/jobplus/handlers/company.py
+++ jobplus/jobplus/handlers/company.py
@@ -1,1 +1,12 @@
-""" 企业列表 企业详情"""
+""" company list,company detail """
+from flask import Blueprint, render_template, redirect, url_for, flash, request, current_app
+from jobplus.models import  User,Job,Company
+from jobplus.forms import LoginForm,RegisterForm,CompanyRegisterForm
+from flask_login import login_user, logout_user, login_required
+
+company = Blueprint('company',__name__,url_prefix='/company')
+
+@company.route('/<int:company_id>')
+def index():
+    company = Company.query.get_or_404(company_id)
+    return render_template('company/detail.html',company=company)
