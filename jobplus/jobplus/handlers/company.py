5dbcaa2040369ffc25e6f86cb8bc21fca2adabc2
503924173 <503924173@qq.com>
2018-03-10 11:29:22 +0800

    merge

---
+++ jobplus/jobplus/handlers/company.py
@@ -0,0 +1,12 @@
+""" company list,company detail """
+from flask import Blueprint, render_template, redirect, url_for, flash, request, current_app
+from jobplus.models import  User,Job,company
+from jobplus.forms import LoginForm,RegisterForm,CompanyRegisterForm
+from flask_login import login_user, logout_user, login_required
+
+company = Blueprint('company',__name__,url_prefix='/company')
+
+@company.route('/<int:company_id>')
+def index():
+    company = Company.query.order_by(Job.created_at)
+    return render_template('company/detail.html',company=company)
